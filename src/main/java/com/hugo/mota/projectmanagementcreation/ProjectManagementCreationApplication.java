6a9ec8fb3b5edf58817f582c6847173daa560b5a
Hugo Mota <hugo.iguanaa@gmail.com>
2021-11-30 11:48:06 -0300

    First Configuration

---
+++ src/main/java/com/hugo/mota/projectmanagementcreation/ProjectManagementCreationApplication.java
@@ -0,0 +1,13 @@
+package com.hugo.mota.projectmanagementcreation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectManagementCreationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectManagementCreationApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hugo/mota/projectmanagementcreation/ProjectManagementCreationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hugo.mota.projectmanagementcreation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProjectManagementCreationApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
